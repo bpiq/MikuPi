a75d3c30e13746cac1ff484575dcdedb016684ee
MikuQ.com <i@mikuq.com>
2016-06-01 23:35:48 +0800

    gpio test

--- MikuPi.h
+++ MikuPi.h
@@ -6,6 +6,19 @@
 #define       FALSE   (1==2)
 #endif
 
+//https://www.arduino.cc/en/Reference/Constants
+
+#define       true    (1)
+#define       false   (0)
+
+#define       HIGH  (1)
+#define       LOW   (0)
+
+#define       INPUT		(0)
+#define       INPUT_PULLUP	(2)
+#define       OUTPUT	(1)
+
+
 #define	PI_MODEL_UNKNOWN	0
 #define	PI_MODEL_M1		1
 #define	PI_MODEL_R1		2
@@ -14,7 +27,7 @@
 #define PI_MODEL_M3     5
 #define PI_MODEL_M2p    6
 
-#define	VERSION "0.20"
+#define	VERSION "0.21"
 
 extern const char *piModelNames    [7] ;
 
@@ -22,5 +35,8 @@ void delay (unsigned int howLong);
 
 void piBoardId(int *model, int *mem);
 void sayhello();
+
+void pinMode(int pin, int mode);
+void digitalWrite(pin, value);
 
 #endif
--- test/hello4.c
+++ test/hello4.c
@@ -2,73 +2,96 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "gpio_lib.h"
 
+// pinToGpio:
+//	Take a Wiring pin (0 through X) and re-map it to the BCM_GPIO pin
+//	Cope for 3 different board revisions here.
 
+static int *wPinToGpio ;
 
-
-int isH3(void)
+static int wPinToGpioM2p [32] =
 {
-  FILE *cpuFd ;
-  char line [120] ;
-  char *d;
-  int processorCount=0;
+	1,16,0,3,		15,68,2,6, 
+	12,11,67,71,	64,65,66,13, 
+	14,-1,-1,-1,	-1,7,8,9, 
+	10,17,354,356,  21,20,19,18
+} ;
 
-  if ((cpuFd = fopen ("/proc/cpuinfo", "r")) == NULL)
-    piBoardRevOops ("Unable to open /proc/cpuinfo") ;
-  while (fgets (line, 120, cpuFd) != NULL)
-  {
-	if (strncmp (line, "processor", 9) == 0)
-      processorCount++;
-    if (strncmp (line, "Hardware", 8) == 0)
-      break;
-  }
-		
-  fclose (cpuFd) ;
-  if (strncmp (line, "Hardware", 8) != 0)
-    piBoardRevOops ("No \"Hardware\" line") ;
-	
-  for (d = &line [strlen (line) - 1] ; (*d == '\n') || (*d == '\r') ; --d)
-    *d = 0 ;
-  if (MikuPiDebug)
-    printf ("piboardRev: Hardware string: %s\n", line) ;
-	
-  if (strstr(line,"sun8i") != NULL)
-  {
-	  if (processorCount==4)
-	  {
-		  //H3-M2P
-	  }
-	  if (processorCount==8)
-	  {
-		  //A83T-M3
-	  }
-    if (MikuPiDebug)
-	{
-      printf ("Hardware:%s\n",line) ;
-      printf ("processorCount:%d\n",processorCount) ;
-	}
-    return TRUE;
-  }
-  else
-  {
-    if (MikuPiDebug)
-      printf ("Hardware:%s\n",line) ;
-    return FALSE;
-  }
-}
+static int wPinToGpioM3 [32] =
+{
+	68,35,71,81,		34,360,361,362, 
+	229,228,67,234,		64,65,66,32, 
+	33,-1,-1,-1,		-1,82,202,203, 
+	204,132,205,133,	146,147,227,226
+} ;
 
+//static int *bPinTowPin;
 
+static int bPinTowPin[41] =
+{
+  -1,		// 0
+  -1, -1,	// 1, 2
+   8, -1,  //3, 4
+   9, -1,  //5, 6
+   7, 15,  //7, 8
+  -1, 16, //9,10
+  0, 1, //11,12
+  2, -1, //13,14
+  3, 4, //15,16
+  -1, 5, //17,18
+  12, -1, //19,20
+   13, 6, //21,22
+  14, 10, //23, 24
+  -1,  11,	// 25, 26
+  
+  30,   31,   //27, 28
+  21,  -1,  //29, 30
+  22,  26,  //31, 32
+  23, -1, //33, 34
+  24, 27, //35, 36
+  25, 28, //37, 38
+  -1, 29, //39, 40
+} ;
 
-
+//#define PA6    SUNXI_GPA(6)  *32+6
+#define PA6    wPinToGpioM2p[bPinTowPin[36]]
 
 int main()
 {
+	/*
+	  if (geteuid () != 0)
+	  {
+    printf("wiringPiSetup: Must be root. (Did you forget sudo?)\n") ;
+	return -1;
+	  }
+	  */
+	    if(SETUP_OK!=sunxi_gpio_init()){
+        printf("Failed to initialize GPIO\n");
+        return -1;
+    }
 
-  /*
-  if (isH3())
-    printf("is H3!\n");
-  else
-    printf("is not H3!\n");
-	*/
+    if(SETUP_OK!=sunxi_gpio_set_cfgpin(PA6,OUTPUT)){
+        printf("Failed to config GPIO pin\n");
+        return -1;
+    }
+
+    int i,j;
+      while(1)	{
+        if(sunxi_gpio_output(PA6,HIGH)){
+            printf("Failed to set GPIO pin value\n");
+            return -1;
+        }
+
+        usleep(500000);
+        if(sunxi_gpio_output(PA6,LOW)){
+            printf("Failed to set GPIO pin value\n");
+            return -1;
+        }
+        usleep(500000);
+    
+}
+    sunxi_gpio_cleanup();
+
   return 0;
 }
